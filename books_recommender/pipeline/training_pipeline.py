0bfe3e1c229bc7aae893af5fbe9c96e9da52669f
SteveKevinDias <diassteve962@gmail.com>
2026-06-22 21:50:52 +0530

    data validation added

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,9 +1,13 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
+from books_recommender.components.stage_01_data_validation import DataValidation
+#rom books_recommender.components.stage_02_data_transformation import DataTransformation
+#from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
 
     def start_training_pipeline(self):
         """
@@ -11,3 +15,4 @@ class TrainingPipeline:
         :return: none
         """
         self.data_ingestion.initiate_data_ingestion()
+        self.data_validation.initiate_data_validation()
